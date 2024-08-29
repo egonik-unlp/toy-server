1f95818c13857033ce6e64d6cfcf4d2cf59cca8d
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2024-08-29 15:39:22 -0300

    funciona

--- test_gap.py
+++ test_gap.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 url =  "http://localhost:3000"
-N = 200
+N = 40
 all_panics =[]
 all_gaps = [0.0001, 0.001, 0.01,0.02,0.03,0.04,0.05,0.06,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.2,1.3,1.4,1.5,2]
 faileds = []
